c9dfde0fd69960420f27d897e84ccf22b4dd0086
jezzamondev <jezzamondev@gmail.com>
2019-09-04 07:46:49 -0400

    CommentBox tets

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = evt => {
+    this.setState({ comment: evt.target.value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+
+    this.setState({ comment: '' });
+  };
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
           <button>Submit Comment</button>
         </div>
