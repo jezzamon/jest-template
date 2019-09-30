aaa2ac5c8f623dcaf264d5270ac2928dafa195e2
jezzamondev <jezzamondev@gmail.com>
2019-09-29 22:11:11 -0400

    feat(server-side-auth): Add passport strategies

---
+++ server/services/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// set up options
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret,
+};
+
+// create jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // see if user id in payload exists in db
+  // if so call done with that user object
+  // otherwise call done without user object
+  User.findById(payload.sub, function(err, user) {
+    if (err) return done(err, false);
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+// Tell passport to use this strategy
+passport.use(jwtLogin);
